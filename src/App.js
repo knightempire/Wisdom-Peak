21fd813b0a5c5d97e434ea0b0984ca9b2adb8127
Abinesh T <108071239+knightempire@users.noreply.github.com>
2025-01-20 11:18:18 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,31 +6,22 @@ import UserDetail from "./components/UserDetail"
 import Navbar from "./components/Navbar"
 
 function App() {
-    return ( <
-        UserProvider >
-        <
-        Router >
-        <
-        div className = "min-h-screen bg-gray-100 dark:bg-gray-900" >
-        <
-        Navbar / >
-        <
-        div className = "container mx-auto px-4 py-8" >
-        <
-        Routes >
-        <
-        Route path = "/"
-        element = { < Home / > }
-        /> <
-        Route path = "/user/:id"
-        element = { < UserDetail / > }
-        /> <
-        /Routes> <
-        /div> <
-        /div> <
-        /Router> <
-        /UserProvider>
-    )
+  return (
+    <UserProvider>
+      <Router>
+        <div className="min-h-screen bg-gray-100 dark:bg-gray-900">
+          <Navbar />
+          <div className="container mx-auto px-4 py-8">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/user/:id" element={<UserDetail />} />
+            </Routes>
+          </div>
+        </div>
+      </Router>
+    </UserProvider>
+  )
 }
 
 export default App
+
